73dd3fbd027ea46d374fae7a2f9d62b159f68c89
isabellang <isabellayln2027@student.cis.edu.hk>
2024-09-09 22:26:25 +0800

    finished lab 2

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,53 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int sum = add(2,5);
+        int sum2 = add2(2,5,8,3);
+        String triples = triple("hello");
+        double half = half(17);
+        double round = roundPositiveValueToNearestInteger(8.5);
+        double roundNegative = roundNegativeValueToNearestInteger(-8.7);
+
+        System.out.println(sum);
+        System.out.println(sum2);
+        System.out.println(morningGreeting("Izzy"));
+        System.out.println(afternoonGreeting("Izzy"));
+        System.out.println(triples);
+        System.out.println(half);
+        System.out.println(round);
+        System.out.println(roundNegative);
+
+    }
+    public static int add(int a, int b){
+        return a+b;
     }
 
-    // 1. add
-    
     // 2. add
-
+    public static int add2(int a, int b, int c, int d){
+        return add(add(a,b),add(c,d));
+    }
     // 3. morningGreeting
-
+    public static String morningGreeting(String name){
+        return "早上好, " + name;
+    }
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String name2){
+        return "下午好, " + name2;
+    }
     // 5. triple
-
+    public static String triple(String triples){
+        return triples + triples + triples;
+    }
     // 6. half
-
+    public static double half(int a){
+        return (double) a/2;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+    public static int roundPositiveValueToNearestInteger(double a){
+        return (int) (a+0.5);
+    }
     // 8. roundNegativeValueToNearestInteger
-
+    public static int roundNegativeValueToNearestInteger(double a){
+        return (int) (a-0.5);
+    }
 }
